121b0e1e0eea6b7f83bf3041005063662f6fa01a
Ri-Hong <riri.hong@gmail.com>
2023-10-14 09:20:21 -0400

    Installed python modules for backend

--- backend/db.py
+++ backend/db.py
@@ -6,7 +6,8 @@ from pymongo.server_api import ServerApi
 def get_db():
     username = os.getenv('MONGODB_USERNAME')
     password = os.getenv('MONGODB_PASSWORD')
-    uri = f"mongodb+srv://{username}:{password}@cluster0.rukfnhr.mongodb.net/?retryWrites=true&w=majority"
+    uri = f"mongodb+srv://{username}:{password}@flaimbrain.q7hbiyx.mongodb.net/?retryWrites=true&w=majority"
+    print(uri)
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client.Database  # Replace 'mydatabase' with your database name
     return db
